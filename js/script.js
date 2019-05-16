070acbcddf66fb85a79066995d926f774c5642f4
Henrik Johansson <henrikjohansson1980@gmail.com>
2019-05-16 09:03:09 +0200

    First

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//alert('Hello');
+const menuButton = document.querySelector("div.menu-button");
+//Set initial state of menu
+/* let showMenu = false;
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    //Add class to menuButton
+    menuButton.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    //Change menu state
+    showMenu = true;
+  } else {
+    //Add class to menuButton
+    menuButton.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    //Change menu state
+    showMenu = false;
+  }
+}
+ */
